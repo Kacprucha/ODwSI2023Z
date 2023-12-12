d77f40ac8a54a788e0c8d64bcba9b869cbbb4c4b
Kacprucha <karas.kacper@gmail.com>
2023-12-12 22:59:02 +0100

    Adding routes to new pages

--- server.py
+++ server.py
@@ -6,7 +6,23 @@ app = Flask(__name__)
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello World"
+    return render_template('index.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/reset')
+def forget_password():
+    return render_template('reset_password.html')
+
+@app.route('/register')
+def register():
+    return render_template('register.html')
+
+@app.route('/home')
+def dashboard():
+    return render_template('home.html', username="Test")
 
 if __name__ == "__main__":
     #serve(app, host="0.0.0.0", port=8000)
