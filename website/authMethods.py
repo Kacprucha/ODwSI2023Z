1ee6b3794c2bf9767349633cc8f7fe61ff1887fa
Kacprucha <karas.kacper@gmail.com>
2023-12-17 20:48:02 +0100

    Adding login/register logic

--- website/authMethods.py
+++ website/authMethods.py
@@ -1,4 +1,13 @@
+from werkzeug.security import generate_password_hash, check_password_hash
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
+from cryptography.hazmat.primitives import hashes
+from cryptography.exceptions import InvalidKey
 import re
+import secrets
+import string
+
+PAPPER = b'papper'
 
 def is_str_complex(password):
     # Check if the password has at least 8 characters
@@ -19,3 +28,42 @@ def is_str_complex(password):
 
     # If all conditions are met, return True
     return True
+
+def hash_password(password, salt):
+    kdf = PBKDF2HMAC(
+        algorithm=hashes.SHA256(),
+        iterations=100000,
+        salt=salt+PAPPER,
+        length=32,
+        backend=default_backend()
+    )
+    password_h = kdf.derive(password.encode('utf-8'))
+    return password_h
+
+def verify_password(password, hashed_password, salt):
+    result = True
+    
+    kdf = PBKDF2HMAC(
+        algorithm=hashes.SHA256(),
+        iterations=100000,
+        salt=salt+PAPPER,
+        length=32,
+        backend=default_backend()
+    )
+
+    try:
+        kdf.verify(password.encode('utf-8'), hashed_password)
+    except InvalidKey as e:
+        result = False
+    else:
+        result = True
+    
+    return result
+
+def generate_random_salt(length):
+    random_numbers = [secrets.randbelow(256) for _ in range(length)]
+    random_bytes = bytes(random_numbers)
+    return random_bytes
+
+def generate_code(length):
+    return str(''.join(secrets.choice('0123456789') for _ in range(length)))
