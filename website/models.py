2c24b0009f00c83acc7880d39f71001b12f6b6f0
Kacprucha <karas.kacper@gmail.com>
2023-12-17 20:48:16 +0100

    Changes in model

--- website/models.py
+++ website/models.py
@@ -6,10 +6,17 @@ class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(150), unique=True)
     password = db.Column(db.String(150))
-    notes = db.relationship('Loan')
+    code = db.Column(db.String(10))
+    loans_as_owner = db.relationship('Loan', back_populates='owner', foreign_keys='Loan.owner_id')
+    loans_as_borrower = db.relationship('Loan', back_populates='borrower', foreign_keys='Loan.borrower_id')
     
 class Loan(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    owner = db.Column(db.Integer, db.ForeignKey('user.id'))
-    borrower = db.Column(db.Integer, db.ForeignKey('user.id'))
+    amount = db.Column(db.Integer)
+    purpose = db.Column(db.String(200))
     date = db.Column(db.DateTime(timezone=True), default=func.now())
+    owner_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    borrower_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+    owner = db.relationship('User', back_populates='loans_as_owner', foreign_keys=[owner_id])
+    borrower = db.relationship('User', back_populates='loans_as_borrower', foreign_keys=[borrower_id])
