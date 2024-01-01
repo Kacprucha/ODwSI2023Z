d34731ee70677cbc7fc131b6b190a509157e414a
Kacprucha <karas.kacper@gmail.com>
2024-01-01 21:32:37 +0100

    Add methods to direct to new pages

--- website/protectedViews.py
+++ website/protectedViews.py
@@ -3,7 +3,17 @@ from flask_login import login_required, current_user
 
 protectedViews = Blueprint('protectedViews', __name__)
 
-@protectedViews.route('/home', methods=['GET', 'POST'])
+@protectedViews.route('/home')
 @login_required
 def home():
     return render_template('home.html', username=current_user.name)
+
+@protectedViews.route('/loan', methods=['GET', 'POST'])
+@login_required
+def loan():
+    return render_template('loan.html')
+
+@protectedViews.route('/pay', methods=['GET', 'POST'])
+@login_required
+def pay():
+    return render_template('pay.html')
