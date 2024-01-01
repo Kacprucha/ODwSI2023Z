f729dd9f72e5c1b03638deed9001273c1b859508
Kacprucha <karas.kacper@gmail.com>
2024-01-01 20:57:34 +0100

    Changing home page

--- website/protectedViews.py
+++ website/protectedViews.py
@@ -3,7 +3,7 @@ from flask_login import login_required, current_user
 
 protectedViews = Blueprint('protectedViews', __name__)
 
-@protectedViews.route('/home')
+@protectedViews.route('/home', methods=['GET', 'POST'])
 @login_required
 def home():
     return render_template('home.html', username=current_user.name)
